3ebb5ba249b28210eb90938eefb0e84fd4adca27
Ayush Yadav <ayush421301@gmail.com>
2024-02-24 02:51:34 +0530

    Commit 2

--- bank_modern_app/src/App.jsx
+++ bank_modern_app/src/App.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
 
-const App = () => {
-  return (
-    <div>App</div>
-  )
-}
+const App = () => (
+    <div className='bg-primary w-full overflow-hidden'>
+      <h1>Hello, World</h1>
+    </div>
+  );
+
 
 export default App
