3ba453aa17fb47560276e91582e21f4456df9a9a
Ayush Yadav <ayush421301@gmail.com>
2024-02-24 17:33:02 +0530

    Commit 5

--- bank_modern_app/src/App.jsx
+++ bank_modern_app/src/App.jsx
@@ -1,26 +1,29 @@
-import styles from "./style"
+import styles from './style'
+
+import { Navbar, Hero ,Stats, Business, Billing ,CardDeal, Testimonials, Clients , CTA, Footer} from './components';
+
 const App = () => (
     <div className='bg-primary w-full overflow-hidden'>
       <div className={'${styles.paddingX} ${styles.flexCentre}'}>
         <div className={'${styles.boxWidth}'}>
-      Navbar
+      <Navbar/>
         </div>
       </div>
     <div className={'bg-primary ${styles.flexStart}'}>
      <div className={'${styles.boxWidth}'}>
-    Hero
+    <Hero/>
      </div>
     </div>
     <div className={'bg-primary ${styles.flexStart} ${styles.paddingX}'}>
      <div className={'${styles.boxWidth}'}>
-      Stats 
-      Businesss 
-      Billing 
-      CardDeal 
-      Testimonials
-      Clients 
-      CTA 
-      Footer 
+      <Stats/> 
+      <Business/> 
+      <Billing/> 
+      <CardDeal/> 
+      <Testimonials/>
+      <Clients />
+      <CTA/>
+      <Footer/>
 
      </div>
     </div>
